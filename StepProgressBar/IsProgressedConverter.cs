713a6dfa9d49f7fa04cafd75b32333ce6c0bb1f2
JustinPihony <justin.pihony@gmail.com>
2013-06-17 23:01:37 -0400

    first commit

---
+++ StepProgressBar/IsProgressedConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace StepProgressBarControl
+{
+    public class IsProgressedConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(values[0] is ContentPresenter && values[1] is Int32))
+                return false;
+            var contentPresenter = values[0] as ContentPresenter;
+            var currentStep = (Int32)values[1];
+            var itemsControl = ItemsControl.ItemsControlFromItemContainer(contentPresenter);
+            var index = itemsControl.ItemContainerGenerator.IndexFromContainer(contentPresenter);
+            return IsThisIndexPastOrCurrentStep(index, currentStep);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private static bool IsThisIndexPastOrCurrentStep(int index, int currentStep)
+        {
+            return index < currentStep;
+        } 
+    }
+}
---
+++ StepProgressBar/StepProgressBar.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace StepProgressBarControl
+{
+    /// <summary>
+    /// Interaction logic for StepProgressBar.xaml
+    /// </summary>
+    public partial class StepProgressBar
+    {
+        #region Dependency Properties
+        public static readonly DependencyProperty NotProgressedBrushProperty =
+          DependencyProperty.Register("NotProgressedBrush", typeof(SolidColorBrush), typeof(StepProgressBar), new PropertyMetadata(new BrushConverter().ConvertFrom("transparent")));
+
+        public SolidColorBrush NotProgressedBrush
+        {
+            get { return (SolidColorBrush)GetValue(NotProgressedBrushProperty); }
+            set { SetValue(NotProgressedBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProgressedBrushProperty =
+          DependencyProperty.Register("ProgressedBrush", typeof(SolidColorBrush), typeof(StepProgressBar), new PropertyMetadata(new BrushConverter().ConvertFrom("green")));
+
+        public SolidColorBrush ProgressedBrush
+        {
+            get { return (SolidColorBrush)GetValue(ProgressedBrushProperty); }
+            set { SetValue(ProgressedBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentStepProperty =
+          DependencyProperty.Register("CurrentStep", typeof(Int32), typeof(StepProgressBar));
+
+        public Int32 CurrentStep
+        {
+            get { return (Int32)GetValue(CurrentStepProperty); }
+            set { SetValue(CurrentStepProperty, value); }
+        }
+
+        public static readonly DependencyProperty TotalStepsProperty =
+          DependencyProperty.Register("TotalSteps", typeof(Int32), typeof(StepProgressBar), new UIPropertyMetadata(TotalStepsChanged));
+
+        public int TotalSteps
+        {
+            get { return (Int32)GetValue(TotalStepsProperty); }
+            set { SetValue(TotalStepsProperty, value); }
+        }
+
+        private static void TotalStepsChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            if (!(dependencyObject is StepProgressBar))
+                return;
+            (dependencyObject as StepProgressBar).TotalStepsAsList = Enumerable.Range(0, (Int32)dependencyPropertyChangedEventArgs.NewValue).ToList();
+        }
+
+        private static readonly DependencyProperty TotalStepsAsListProperty =
+          DependencyProperty.Register("TotalStepsAsList", typeof(IEnumerable<Int32>), typeof(StepProgressBar));
+
+        private IEnumerable<Int32> TotalStepsAsList
+        {
+            get { return (IEnumerable<Int32>)GetValue(TotalStepsAsListProperty); }
+            set { SetValue(TotalStepsAsListProperty, value); }
+        }
+        #endregion
+
+        public StepProgressBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
